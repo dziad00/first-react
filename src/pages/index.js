b07299c99a5fce1c77163862520dcdb70d8f8b03
Janusz Szewczyk <Janusz@Januszs-MacBook-Pro.local>
2019-05-21 09:02:48 +0200

    second_lesson

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome to Bello Bello</h1>
-    <p>Experience designer with 10 years of experience in design and build products.</p>
-    <Link to="/page-2/">Contact me</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Welcome to Bello Bello</h1>
+        <p>Experience designer with 10 years of experience in design and build products.</p>
+        <Link to="/page-2/">Contact me</Link>
+      </div>
+    </div>
   </div>
 )
 
